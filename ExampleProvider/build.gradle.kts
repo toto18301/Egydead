b60c68c3e8593e6704e05af1dd6a9f97881997b2
toto18301 <toufiktoo03@gmail.com>
2025-08-22 19:21:28 +0200

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,24 +1,35 @@
 // use an integer for version numbers
 version = 1
 
-
+// CloudStream plugin metadata
 cloudstream {
-    // All of these properties are optional, you can safely remove them
+    // Optional but helpful metadata
+    description = "EgyDead (tv2.egydead.live) source"
+    authors = listOf("Toufik Too")
+    status = 1 // 0:Down, 1:Ok, 2:Slow, 3:Beta only
 
-    description = "Lorem Ipsum"
-    authors = listOf("Cloudburst")
-
-    /**
-    * Status int as the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta only
-    * */
-    status = 1 // will be 3 if unspecified
-
-    // List of video source types. Users are able to filter for extensions in a given category.
-    // You can find a list of avaliable types here:
-    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
-    tvTypes = listOf("Others")
+    // What types your provider offers (you can widen later)
+    tvTypes = listOf("Movie", "TvSeries")
 }
+
+// Android Gradle Plugin 8.x requires a namespace per module.
+// This MUST match the package at the top of your ExampleProvider.kt file.
+// If your Kotlin file starts with `package com.example`, leave this as is.
+android {
+    namespace = "com.example"
+
+    // These can mirror your root settings. It's fine to repeat here.
+    compileSdk = 35
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 35
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
