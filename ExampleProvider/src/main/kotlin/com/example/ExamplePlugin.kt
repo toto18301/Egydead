a88ee812e8e097567dfead35a96487c7703f5a4c
toto18301 <toufiktoo03@gmail.com>
2025-08-22 19:29:49 +0200

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -1,13 +1,11 @@
 package com.example
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class TestPlugin: Plugin() {
-    override fun load(context: Context) {
-        // All providers should be added in this manner. Please don't edit the providers list directly.
+class ExamplePlugin {
+    fun load(context: Context) {
+        // Register our provider class above
         registerMainAPI(ExampleProvider())
     }
-}
